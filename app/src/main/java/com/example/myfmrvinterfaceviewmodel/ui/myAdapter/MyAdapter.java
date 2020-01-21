d1bedfc7830191617527b8eded4af0a8fccf9012
WeZaPro <taweesak9359@gmail.com>
2020-01-21 11:35:19 +0700

    update-210163-2

---
+++ app/src/main/java/com/example/myfmrvinterfaceviewmodel/ui/myAdapter/MyAdapter.java
@@ -0,0 +1,12 @@
+package com.example.myfmrvinterfaceviewmodel.ui.myAdapter;
+
+public class MyAdapter {
+
+    // Adapter
+    // ViewHolder
+
+    // listener Send data to MainActivity
+    // onAttact
+    // deAttact
+
+}
